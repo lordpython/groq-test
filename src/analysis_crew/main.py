7622dcbbcad918e160f3ace35bb9f82a123ac96c
Yousef Alsalem <admin@kwtgpt.com>
2024-06-28 09:43:16 +0300

    .

--- src/analysis_crew/main.py
+++ src/analysis_crew/main.py
@@ -2,30 +2,30 @@
 from analysis_crew.analysis_crew import AnalysisCrew
 from analysis_crew.report_generation_crew import ReportGeneratorCrew
 
-
 def run():
     inputs = {
         'file_path': 'src/analysis_crew/files/tesla.csv'
     }
 
     report = []
-    report_plan = AnalysisCrew().crew().kickoff(inputs=inputs)
-
-    print("Report Plan: ", report_plan.json())
-
-    for section in report_plan.sections:
-        plan = section.json()
-        inputs = {'plan': plan}
-        report_generator_crew = ReportGeneratorCrew()
-        report_section = report_generator_crew.crew().kickoff(inputs=inputs)
-        report.append(report_section)
-
-    r = '\n\n'.join(report)
-    with open('report.md', 'w') as f:
-        f.write(r)
-
-    print(r)
-
+    try:
+        report_plan = AnalysisCrew().crew().kickoff(inputs=inputs)
+        print("Report Plan: ", report_plan.json())
+    
+        for section in report_plan.sections:
+            plan = section.json()
+            inputs = {'plan': plan}
+            report_generator_crew = ReportGeneratorCrew()
+            report_section = report_generator_crew.crew().kickoff(inputs=inputs)
+            report.append(report_section)
+    
+        r = '\n\n'.join(report)
+        with open('report.md', 'w') as f:
+            f.write(r)
+    
+        print(r)
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     run()
